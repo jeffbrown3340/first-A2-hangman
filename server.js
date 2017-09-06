2c99853be99adf08356860b3107402784f726c28
JeffB <jeff@jeffbrown.us>
2017-09-06 00:02:47 -0400

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var fs = require("fs");
+var PORT = 7822;
+var server = http.createServer(handleRequest);
+function handleRequest(req, res) {
+  fs.readFile("index.html", function(err, data) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+}
+server.listen(PORT, function() {
+  console.log("Server is listening on PORT: " + PORT);
+});
